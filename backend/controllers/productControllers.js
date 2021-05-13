7d2eaaa6eb21054d009250bd84fc49b9a581c273
joaquin.caceres <jcg230795@gmail.com>
2021-05-14 01:20:14 +0200

    ..

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,28 @@
+const Product = require('../models/product')
+
+//create new product => /api/v1/product/new
+
+exports.newProduct = async (req, res, next) => {
+
+    const product = await Product.create(req.body)
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+
+
+}
+
+//get all product => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find()
+
+
+    res.status(200).json({
+        success: true,
+        count: products.length,
+        products
+    })
+}
