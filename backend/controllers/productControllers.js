1a118ec8b023c91ba8e4e8fcda26e361d6ec455d
joaquin.caceres <jcg230795@gmail.com>
2021-05-31 20:14:24 +0200

    get single product, get all products, delete product, update product...

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product')
 
-//create new product => /api/v1/product/new
+
+//create new product => /api/v1/admin/product/new
 
 exports.newProduct = async (req, res, next) => {
 
@@ -25,4 +26,64 @@ exports.getProducts = async (req, res, next) => {
         count: products.length,
         products
     })
+}
+
+//get single product => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next)  => {
+
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+
+        res.status(404).json({
+            success: false,
+            message: "product not found"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
+
+//update product => /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true, 
+        product
+    })
+}
+
+//Delete Product   => /api/v1/admin/product/:id
+
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+
+        res.status(404).json({
+            success: false,
+            message: "product not found"
+        })
+    }
+
+    await product.remove()
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted'
+    })
+
+
 }
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { getProducts, newProduct } = require('../controllers/productControllers')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productControllers')
 
 router.route('/products').get(getProducts)
-router.route('/product/new').post(newProduct)
+router.route('/product/:id').get(getSingleProduct)
+router.route('/admin/product/new').post(newProduct)
+router.route('/admin/product/:id').put(updateProduct)
+router.route('/admin/product/:id').delete(deleteProduct)
+
 
 
 module.exports = router
